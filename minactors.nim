d71524eb8eda967b160c639c28afa5edff109062
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2017-01-19 15:52:25 +0100

    fix: simple round-robin scheduler

--- minactors.nim
+++ minactors.nim
@@ -6,9 +6,11 @@ type
     effect: Behavior -> Behavior
   Behavior = object
     behavior: Message -> Effect
-  Address  = object
+  Address  = ref object
     mailbox: Queue[Message]
     receive: Behavior
+  ActorSystem = object
+    addresses: seq[Address]
 
 
 let Stay   = Effect( effect: proc (old: Behavior): Behavior = old )
@@ -24,30 +26,38 @@ let Die =
     )
   )
 
-proc Actor(initial: Behavior): Address =
-  Address(
+
+
+proc actorOf(self: var ActorSystem, initial: Behavior): Address =
+  var address = Address(
     mailbox: initQueue[Message](),
     receive: initial
   )
+  self.addresses.add(address)
+  address
 
 proc `!`(self: var Address, m: Message) =
   self.mailbox.add(m)
 
-proc run(self: var Address): void =
-  let eff = self.receive.behavior(self.mailbox.pop())
-  self.receive = eff.effect(self.receive)
-  if self.mailbox.len != 0:
-    self.run()
+proc run(self: var ActorSystem): void =
+  while true:
+    for i in 0..self.addresses.len-1:
+      var actorRef = self.addresses[0]
+      if (actorRef.mailbox.len != 0):
+        let eff = actorRef.receive.behavior(actorRef.mailbox.pop())
+        actorRef.receive = eff.effect(actorRef.receive)
 
 var
- actor = Actor(Behavior(behavior:proc(m:Message): Effect =
+ system = ActorSystem(addresses: @[])
+ actor = system.actorOf(Behavior(behavior:proc(m:Message): Effect =
     echo "ciao ", m
     Stay
   ))
+
 
 actor ! 1
 actor ! 2
 actor ! 3
 actor ! 4
 
-run(actor)
+system.run()
