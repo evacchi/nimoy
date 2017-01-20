060da61b18146b06a59b611fdb82febe9129a120
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2017-01-20 12:31:14 +0100

    context become

--- minactors.nim
+++ minactors.nim
@@ -1,6 +1,12 @@
 import future, queues, threadpool
 
+{.experimental.}
+
 type
+  Flaffo = ref object
+    mailbox: ref seq[int]
+
+
   Message  = ref object
     value: int
     sender: Address
@@ -8,16 +14,27 @@ type
     effect: Behavior -> Behavior
   Behavior = object
     behavior: Message -> Effect
-  Address  = ref object
-    mailbox: Queue[Message]
+  AddressObj = object
+    mailbox: ref Queue[Message]
     receive: Behavior
+  Address  = ref AddressObj
   ActorSystem = ref object
     addresses: seq[Address]
 
 
-let Stay   = Effect( effect: proc (old: Behavior): Behavior = old )
 proc Become(newB: Behavior): Effect =
   Effect( effect: proc (old: Behavior): Behavior = newB )
+
+proc become(behavior: proc(m: Message):Effect): Effect =
+  let b = Behavior(behavior: behavior)
+  Effect( effect: proc (old: Behavior): Behavior = b )
+
+
+proc behavior(behavior: proc(m: Message):Effect): Behavior =
+  Behavior(behavior: behavior)
+
+
+let Stay   = Effect(effect: proc (old: Behavior): Behavior = old )
 
 
 let Die =
@@ -28,60 +45,77 @@ let Die =
     )
   )
 
-proc actorOf(self: var ActorSystem, initial: Behavior): Address =
-  var address: Address
-  address = Address(
-    mailbox: initQueue[Message](),
-    receive: initial
-  )
+proc actorOf(self: var ActorSystem, initial: Address -> Behavior): Address =
+  var address: Address = new AddressObj
+  address.mailbox = new Queue[Message]
+  address.mailbox[] = initQueue[Message]()
+  address.receive = initial(address)
   self.addresses.add(address)
   address
+
+proc makeActor(self: var ActorSystem, initial: (Address, Message) -> Effect): Address =
+  self.actorOf( 
+    proc (address: Address): Behavior = 
+      behavior do (message: Message) -> Effect: 
+        initial(address, message)
+  )
 
 proc `!`(self: var Address, m: Message) =
   self.mailbox.add(m)
   # echo "received: ", self.mailbox
 
 proc processActor(self: Address) =
-  if (self.mailbox.len != 0):
-    let msg = self.mailbox.pop()
+  if (self.mailbox[].len != 0):
+    let msg = self.mailbox[].pop()
     let eff = self.receive.behavior(msg)
-    self.receive = eff.effect(self.receive)
+    let beh = eff.effect(self.receive)
+    self.receive = beh
 
 var workQueue = initQueue[Address]()
 
 proc run(self: ActorSystem): void =
   while true:
-    # while workQueue.len != 0:
-    #    workQueue.pop().processActor()
+    while workQueue.len != 0:
+      let aref = workQueue.pop()
+      processActor(aref)
     for i in 0..self.addresses.len-1:
       var actorRef = self.addresses[i]
-      if (actorRef.mailbox.len != 0):
-        # workQueue.add(actorRef)
-        processActor(actorRef)
-
+      if (actorRef.mailbox[].len != 0):
+        workQueue.add(actorRef)
+        # processActor(actorRef)
 
 
 var
   system = ActorSystem(addresses: @[])
-  actor1: Address
-  actor2: Address
+  actor1 = system.makeActor do (self: Address, m: Message) -> Effect:
+    echo "received first message"
+    m.sender ! Message(value: m.value+1, sender: self)
+    # actor2 ! Message(value: 5, sender: actor1)
+    become do (m: Message) -> Effect:
+      echo "actor1: ", m.value
+      m.sender ! Message(value: m.value+1, sender: self)
+      Stay
+    
 
-actor1 = system.actorOf(Behavior(behavior:proc(m:Message): Effect =
-  echo "actor1: ", m.value
-  actor2 ! Message(value: m.value + 1, sender: actor1)
-  Stay
-))
-actor2 = system.actorOf(Behavior(behavior:proc(m:Message): Effect =
- echo "actor2: ", m.value
- actor1 ! Message(value: m.value + 1, sender: actor2)
- Stay
-))
-
-
-actor1 ! Message(value: 1, sender: actor2)
-
-system.run()
+  actor2 = system.makeActor do (self: Address, m: Message) -> Effect:
+    echo "actor2: ", m.value
+    m.sender ! Message(value: m.value + 1, sender: self)
+    Stay
 
 
-while true:
-  discard
+proc mainLoop() =
+  # var thread: Thread[ActorSystem]
+  # createThread(thread, run, system)
+  system.run
+  while true:
+    discard
+
+
+
+
+# system.run()
+actor1 ! Message(value: 1, sender: actor1)
+actor2 ! Message(value: 3, sender: actor2)
+
+mainLoop()
+
