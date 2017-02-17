92b33942979c09fedcee3fd082efdb1374229cf0
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2017-02-17 22:02:44 +0100

    working nested become with mutable state

--- nimoy.nim
+++ nimoy.nim
@@ -4,7 +4,7 @@ type
   ActorId = int
   
   ActorRef = object
-    path: ActorId 
+    id: ActorId 
 
   ActorSystem = object 
     table: SharedTable[ActorId, Actor]
@@ -25,7 +25,7 @@ type
   ActorInit     = proc(context: var ActorContext)
 
   Actor = object
-    path: ActorId
+    id: ActorId
     mailbox: Deque[Envelope]
     behavior: ActorBehavior
 
@@ -39,31 +39,32 @@ proc become(context: var ActorContext, newBehavior: ActorBehavior) =
 
 
 proc processContext(system: var ActorSystem, currentContext: var ActorContext) =
-  system.table.withValue(currentContext.self.path, actor):
+  system.table.withValue(currentContext.self.id, actor):
     if actor.mailbox.len > 0:
+      currentContext.behavior = actor.behavior
       let e = actor.mailbox.popFirst()
       actor.behavior(currentContext, e)
       actor.behavior = currentContext.behavior 
 
   for e in currentContext.outbox:
-    system.table.withValue(e.receiver.path, actor):
+    system.table.withValue(e.receiver.id, actor):
       actor.mailbox.addLast(e)
 
   
 
-proc createActor(system: var ActorSystem, path: ActorId, 
+proc createActor(system: var ActorSystem, id: ActorId, 
                  init: ActorInit): ActorRef =
   var actor = Actor(mailbox: initDeque[Envelope]())
-  var actorRef = ActorRef(path: path)
+  var actorRef = ActorRef(id: id)
   var currentContext = ActorContext(self: actorRef, outbox: initDeque[Envelope]()) 
   init(currentContext)
   actor.behavior = currentContext.behavior
-  system.table[path] = actor
+  system.table[id] = actor
   system.processContext(currentContext)
   actorRef
 
-proc process(system: var ActorSystem, path: ActorId) =
-  let currentRef = ActorRef(path: path)
+proc process(system: var ActorSystem, id: ActorId) =
+  let currentRef = ActorRef(id: id)
   var currentContext = ActorContext(self: currentRef, outbox: initDeque[Envelope]()) 
   system.processContext(currentContext)
 
@@ -80,9 +81,16 @@ let fooRef = system.createActor(100) do (context: var ActorContext):
 let barRef = system.createActor(200) do (context: var ActorContext):
   writeLine(stdout, "startup 200")
   context.send(Message(1), fooRef)
+
+  var i = 1000
   context.become do (context: var ActorContext, e: Envelope):
     writeLine(stdout, context.self, " has received ", e.message, " from ", e.sender)
     context.send(Message(e.message + 1), e.sender)
+    i = i - 1
+    if (i <= 100):
+      context.become do (context: var ActorContext, e: Envelope):
+        writeLine(stdout, "DONE.")
+        
   
 
 
@@ -95,11 +103,11 @@ var t1,t2: Thread[void]
 
 createThread(t1, proc() {.thread.} = 
   while true:
-    system.process(fooRef.path)
+    system.process(fooRef.id)
 )
 createThread(t2, proc() {.thread.} = 
   while true:
-    system.process(barRef.path)
+    system.process(barRef.id)
 )
 
 
