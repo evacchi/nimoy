3ca9eb2d01e4daff4a650b8369d68c2e66513726
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2017-02-18 14:14:44 +0100

    refactoring

--- nimoy.nim
+++ nimoy.nim
@@ -29,99 +29,123 @@ type
     mailbox: Deque[Envelope]
     behavior: ActorBehavior
 
+proc nop(context: var ActorContext, envelope: Envelope) =
+  writeLine(stdout, context.self, ": Unitialized actor could not handle message ", envelope)
 
 proc send(context: var ActorContext, message: Message, receiver: ActorRef) =
-    let e = Envelope(message: message, sender: context.self, receiver: receiver)
-    context.outbox.addLast(e)
-
-proc nop(context: var ActorContext, envelope: Envelope) =
-  writeLine(stdout, context.self, ": Unhandled message ", envelope)
+  let e = Envelope(message: message, sender: context.self, receiver: receiver)
+  context.outbox.addLast(e)
 
 proc become(context: var ActorContext, newBehavior: ActorBehavior) =
-    context.behavior = newBehavior
+  context.behavior = newBehavior
+
+proc processContextOutbox(system: var ActorSystem, currentContext: var ActorContext) =
+  for e in currentContext.outbox:
+    system.table.withValue(e.receiver.id, actor):
+      actor.mailbox.addLast(e)
 
 
 proc processContext(system: var ActorSystem, currentContext: var ActorContext) =
   system.table.withValue(currentContext.self.id, actor):
-    if actor.mailbox.len > 0:
+    while actor.mailbox.len > 0:
       currentContext.behavior = actor.behavior
       let e = actor.mailbox.popFirst()
       actor.behavior(currentContext, e)
       actor.behavior = currentContext.behavior 
 
-  for e in currentContext.outbox:
-    system.table.withValue(e.receiver.id, actor):
-      actor.mailbox.addLast(e)
+  system.processContextOutbox(currentContext)
 
-  
+
 proc createActorContext(actorRef: ActorRef): ActorContext =
   ActorContext(self: actorRef, outbox: initDeque[Envelope](), behavior: nop) 
 
-proc createActor(system: var ActorSystem, id: ActorId, 
-                 init: ActorInit): ActorRef =
-  var actor = Actor(mailbox: initDeque[Envelope]())
-  var actorRef = ActorRef(id: id)
-  var currentContext = createActorContext(actorRef)
-  init(currentContext)
-  actor.behavior = currentContext.behavior
-  system.table[id] = actor
-  system.processContext(currentContext)
-  actorRef
-
-proc process(system: var ActorSystem, id: ActorId) =
-  let actorRef = ActorRef(id: id)
+proc process(system: var ActorSystem, actorRef: ActorRef) =
   var currentContext = createActorContext(actorRef)
   system.processContext(currentContext)
 
 proc createActorSystem(): ActorSystem =
   ActorSystem(table: initSharedTable[ActorId, Actor]())
 
-var system = createActorSystem()
-let fooRef = system.createActor(100) do (context: var ActorContext):
-  writeLine(stdout, "startup 100")
-
-  proc receive(context: var ActorContext, e: Envelope) =
-    writeLine(stdout, context.self, " has received ", e.message, " from ", e.sender)
-    context.send(Message(e.message + 1), e.sender)
-
-  context.become(receive)
-
-
-
-let barRef = system.createActor(200) do (context: var ActorContext):
-  writeLine(stdout, "startup 200")
-  context.send(Message(1), fooRef)
-
-  # state
-  var i = 1000
-
-  proc receive2(context: var ActorContext, e: Envelope) =
-    writeLine(stdout, "DONE.")
-
-  proc receive(context: var ActorContext, e: Envelope) =
-    writeLine(stdout, context.self, " has received ", e.message, " from ", e.sender)
-    context.send(Message(e.message + 1), e.sender)
-    i = i - 1
-    if (i <= 100):
-      context.become(receive2)
-        
-  
-  context.become(receive)
+proc createActor(system: var ActorSystem, id: ActorId, 
+                 init: ActorInit): ActorRef =
+  var actor = Actor(id: id, mailbox: initDeque[Envelope](), behavior: nop)
+  var actorRef = ActorRef(id: id)
+  var currentContext = createActorContext(actorRef)
+  init(currentContext)
+  actor.behavior = currentContext.behavior
+  system.table[id] = actor
+  system.processContextOutbox(currentContext)
+  actorRef
 
 
-var t1,t2: Thread[void]
+when isMainModule:
+
+
+  var system = createActorSystem()
+
+  let fooRef = system.createActor(100) do (context: var ActorContext):
+    writeLine(stdout, "startup 100")
+
+    proc receive(context: var ActorContext, e: Envelope) =
+      writeLine(stdout, context.self, " has received ", e.message, " from ", e.sender)
+      context.send(Message(e.message + 1), e.sender)
+
+    context.become(receive)
+
+  let barRef = system.createActor(200) do (context: var ActorContext):
+    writeLine(stdout, "startup 200")
+    context.send(Message(1), fooRef)
+
+    # state
+    var i = 1000
+
+    proc done(context: var ActorContext, e: Envelope) =
+      writeLine(stdout, "DONE.")
+
+    proc receive(context: var ActorContext, e: Envelope) =
+      writeLine(stdout, context.self, " has received ", e.message, " from ", e.sender)
+      context.send(Message(e.message + 1), e.sender)
+      i = i - 1
+      if (i <= 100):
+        context.become(done)
+          
+    
+    context.become(receive)
+
+  let bazRef = system.createActor(300) do (context: var ActorContext):
+    writeLine(stdout, "startup 300")
+    context.send(Message(3), fooRef)
+
+    var i = 1000
+
+    proc done(context: var ActorContext, e: Envelope) =
+      writeLine(stdout, "DONE.")
+
+    proc receive(context: var ActorContext, e: Envelope) =
+      writeLine(stdout, context.self, " has received ", e.message, " from ", e.sender)
+      context.send(Message(e.message + 1), e.sender)
+      i = i - 1
+      if (i <= 100):
+        context.become(done)
+          
+    
+    context.become(receive)
 
 
 
-createThread(t1, proc() {.thread.} = 
-  while true:
-    system.process(fooRef.id)
-)
-createThread(t2, proc() {.thread.} = 
-  while true:
-    system.process(barRef.id)
-)
+  var t1,t2,t3: Thread[void]
 
 
 
-joinThreads(t1,t2)
+  createThread(t1, proc() {.thread.} = 
+    while true:
+      system.process(fooRef)
+      system.process(barRef)
+      system.process(bazRef)
+  )
+
+
+  joinThreads(t1)
+
+
+
