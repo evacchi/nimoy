9a02965e9ddb7a27ddab0526a507cd6a92c24298
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2017-02-18 23:03:40 +0100

    working executor

--- nimoy.nim
+++ nimoy.nim
@@ -2,39 +2,47 @@ import sharedtables, deques
 
 type
   ActorId = int
-  
-  ActorRef = object
-    id: ActorId 
-
-  ActorSystem = object 
-    table: SharedTable[ActorId, Actor]
-
-  ActorContext = object
-    self: ActorRef
-    outbox: Deque[Envelope]
-    behavior: ActorBehavior
-  
-  Message = int
-  
-  Envelope = object
-    message: Message
-    sender: ActorRef
-    receiver: ActorRef
-
-  ActorBehavior = proc(context: var ActorContext, envelope: Envelope)
-  ActorInit     = proc(context: var ActorContext)
 
   Actor = object
     id: ActorId
     mailbox: Deque[Envelope]
     behavior: ActorBehavior
 
+  ActorRef = object
+    id: ActorId
+
+  Message = int
+
+  Envelope = object
+    message: Message
+    sender: ActorRef
+    receiver: ActorRef
+
+
+  ActorContext = object
+    self: ActorRef
+    outbox: seq[Envelope]
+    behavior: ActorBehavior
+    system: ActorSystem
+
+  ActorBehavior =
+    proc(context: var ActorContext, envelope: Envelope)
+
+  ActorInit =
+    proc(context: var ActorContext)
+
+  ActorSystem = object
+    table: SharedTable[ActorId, Actor]
+
+
+
+
 proc nop(context: var ActorContext, envelope: Envelope) =
   writeLine(stdout, context.self, ": Unitialized actor could not handle message ", envelope)
 
 proc send(context: var ActorContext, message: Message, receiver: ActorRef) =
   let e = Envelope(message: message, sender: context.self, receiver: receiver)
-  context.outbox.addLast(e)
+  context.outbox.add(e)
 
 proc become(context: var ActorContext, newBehavior: ActorBehavior) =
   context.behavior = newBehavior
@@ -44,43 +52,60 @@ proc processContextOutbox(system: var ActorSystem, currentContext: var ActorCont
     system.table.withValue(e.receiver.id, actor):
       actor.mailbox.addLast(e)
 
-
 proc processContext(system: var ActorSystem, currentContext: var ActorContext) =
   system.table.withValue(currentContext.self.id, actor):
     while actor.mailbox.len > 0:
       currentContext.behavior = actor.behavior
       let e = actor.mailbox.popFirst()
       actor.behavior(currentContext, e)
-      actor.behavior = currentContext.behavior 
+      actor.behavior = currentContext.behavior
 
   system.processContextOutbox(currentContext)
 
 
-proc createActorContext(actorRef: ActorRef): ActorContext =
-  ActorContext(self: actorRef, outbox: initDeque[Envelope](), behavior: nop) 
+proc createActorContext(system: var ActorSystem, actorRef: ActorRef): ActorContext =
+  ActorContext(self: actorRef, outbox: @[], behavior: nop, system: system)
 
 proc process(system: var ActorSystem, actorRef: ActorRef) =
-  var currentContext = createActorContext(actorRef)
+  var currentContext = system.createActorContext(actorRef)
   system.processContext(currentContext)
 
 proc createActorSystem(): ActorSystem =
   ActorSystem(table: initSharedTable[ActorId, Actor]())
 
-proc createActor(system: var ActorSystem, id: ActorId, 
+proc createActor(system: var ActorSystem, id: ActorId,
                  init: ActorInit): ActorRef =
   var actor = Actor(id: id, mailbox: initDeque[Envelope](), behavior: nop)
   var actorRef = ActorRef(id: id)
-  var currentContext = createActorContext(actorRef)
+  var currentContext = system.createActorContext(actorRef)
   init(currentContext)
   actor.behavior = currentContext.behavior
   system.table[id] = actor
   system.processContextOutbox(currentContext)
   actorRef
 
+proc createActor(context: var ActorContext, id: ActorId,
+                 init: ActorInit): ActorRef =
+  context.system.createActor(id, init)
+
+type
+  Task = proc() {.thread,noSideEffect.}
+  Executor = object
+    tasks: seq[Task]
+
+proc submit(executor: var Executor, task: Task) =
+  executor.tasks.add(task)
+
+proc start(executor: var Executor) =
+  var t1: Thread[Executor]
+  createThread(t1, (proc(executor: Executor) {.thread.} =
+    for t in executor.tasks:
+      t()
+  ), executor)
+  joinThread(t1)
+
 
 when isMainModule:
-
-
   var system = createActorSystem()
 
   let fooRef = system.createActor(100) do (context: var ActorContext):
@@ -108,8 +133,8 @@ when isMainModule:
       i = i - 1
       if (i <= 100):
         context.become(done)
-          
-    
+
+
     context.become(receive)
 
   let bazRef = system.createActor(300) do (context: var ActorContext):
@@ -127,25 +152,33 @@ when isMainModule:
       i = i - 1
       if (i <= 100):
         context.become(done)
-          
-    
+
+
     context.become(receive)
 
 
 
-  var t1,t2,t3: Thread[void]
+  # var t1,t2,t3: Thread[void]
 
 
-
-  createThread(t1, proc() {.thread.} = 
+  var executor = Executor(tasks: @[])
+  executor.submit(Task(proc() {.thread.} =
     while true:
       system.process(fooRef)
       system.process(barRef)
       system.process(bazRef)
-  )
+  ))
+
+  executor.start()
 
 
-  joinThreads(t1)
+
+  # createThread(t1, proc() {.thread.} =
+  #  while true:
+  #    system.process(fooRef)
+  #    system.process(barRef)
+  #    system.process(bazRef)
+  # )
 
 
-
+  # joinThreads(t1)
