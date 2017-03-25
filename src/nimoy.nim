b1beadfc47842b7459e099fbc79ffe3ab0870431
Vacchi Edoardo (UniCredit) <edoardo.vacchi@unicredit.eu>
2017-03-25 14:05:26 +0100

    become works

--- src/nimoy.nim
+++ src/nimoy.nim
@@ -6,12 +6,9 @@ type
   Actor = object
     id: ActorId
     mailbox: SharedChannel[Envelope]
+    system:  ActorSystem
     behavior: ActorBehavior
-    system:   ActorSystem
 
-  ActorRef = object
-    id: ActorId
-    mailbox: SharedChannel[Envelope]
 
   Message = int
 
@@ -49,24 +46,83 @@ proc send(context: var ActorContext, message: Message, receiver: Actor) =
 proc become(context: var ActorContext, newBehavior: ActorBehavior) =
   context.behavior = newBehavior
 
-proc createActorContext(system: ActorSystem, actorRef: Actor): ActorContext =
-  ActorContext(self: actorRef, behavior: nop, system: system)  
+proc createActorContext(system: ActorSystem, actor: Actor): ActorContext =
+  ActorContext(self: actor, behavior: actor.behavior, system: system)  
 
 proc createActorSystem(): ActorSystem =
   ActorSystem()
 
-proc createActor(system: var ActorSystem, id: ActorId, init: ActorInit): Actor =
-  var actor = Actor(id: id, mailbox: newSharedChannel[Envelope](), behavior: nop)
-  var currentContext = system.createActorContext(actor)
-  init(currentContext)
-  actor.behavior = currentContext.behavior
-  actor
+# proc createActor(system: var ActorSystem, id: ActorId, init: ActorInit): Actor =
+#   var actor = Actor(id: id, mailbox: newSharedChannel[Envelope](), behavior: nop)
+#   var currentContext = system.createActorContext(actor)
+#   init(currentContext)
+#   actor.behavior = currentContext.behavior
+#   actor
 
 proc send(actor: Actor, envelope: Envelope) = 
   actor.mailbox.send(envelope)
 
+
+template createTask(initActorBody: untyped): auto =
+  let r = (block:
+    var channel = newSharedChannel[Envelope]()
+    var receive: ActorBehavior = initActorBody
+    var actor = Actor(mailbox: channel, behavior: receive)
+    echo $(actor)
+    let t = proc() {.thread.} =
+      let (hasMsg, msg) = actor.mailbox.tryRecv()
+      var context = actor.system.createActorContext(actor)
+      if hasMsg:
+        echo "receive:"
+        context.behavior(context, msg)
+        actor.behavior = context.behavior
+        
+    (actor, t))
+  r
+
+
 when isMainModule:
   var system = createActorSystem()
+  var executor = createExecutor()        
+
+  let (foo, tfoo) = (
+    block:
+      createTask:
+        var count = 0
+        proc done(context: var ActorContext, e: Envelope) =
+          writeLine(stdout, "DONE.")
+
+        proc receive(context: var ActorContext, e: Envelope) =
+          writeLine(stdout, 
+            "foo has received ", e.message, " from ", e.sender)
+          e.sender.mailbox.send(Envelope(message: e.message + 1, sender: context.self))
+          count += 1
+          if count >= 10:
+            context.become(done)
+
+        receive
+  )
+
+  let (bar, tbar) = (
+    block:
+      createTask:
+        proc receive(context: var ActorContext, e: Envelope) =
+          writeLine(stdout, 
+            "bar has received ", e.message, " from ", e.sender)
+          e.sender.send(Envelope(message: e.message + 1, sender: context.self))
+          e.sender.send(Envelope(message: e.message - 1, sender: context.self))
+
+        receive
+  )
+
+
+
+  executor.submit(tfoo)
+  executor.submit(tbar)
+
+  bar.send(Envelope(message: 1, sender: foo))
+
+  executor.start()
 
 
   # let barRef = system.createActor(200) do (context: var ActorContext):
@@ -118,52 +174,4 @@ when isMainModule:
 
 
 
-  template createTask(initActorBody: untyped): auto =
-    let r = (block:
-      var channel = newSharedChannel[Envelope]()
-      var actor = Actor(mailbox: channel)
-      echo $(actor)
-      let t = proc() {.thread.} =
-        let receive: ActorBehavior = initActorBody
-        let (hasMsg, msg) = actor.mailbox.tryRecv()
-        var context = actor.system.createActorContext(actor)
-        if hasMsg:
-          echo "receive:"
-          receive(context, msg)
-          #receive = context.behavior
-          
-      (actor, t))
-    r
-
   
-  var executor = createExecutor()        
-
-  let (foo, tfoo) = (
-    block:
-      createTask:
-        proc receive(context: var ActorContext, e: Envelope) =
-          writeLine(stdout, 
-            "foo has received ", e.message, " from ", e.sender)
-          e.sender.mailbox.send(Envelope(message: e.message + 1, sender: context.self))
-        receive
-      )
-
-  let (bar, tbar) = (
-    block:
-      createTask:
-        proc receive(context: var ActorContext, e: Envelope) =
-          writeLine(stdout, 
-            "bar has received ", e.message, " from ", e.sender)
-          e.sender.send(Envelope(message: e.message + 1, sender: context.self))
-          e.sender.send(Envelope(message: e.message - 1, sender: context.self))
-        receive
-      )
-
-
-
-  executor.submit(Task(tfoo))
-  executor.submit(Task(tbar))
-
-  bar.send(Envelope(message: 1, sender: foo))
-
-  executor.start()
