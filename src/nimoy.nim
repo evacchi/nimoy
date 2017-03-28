ec4e946fc0ab48ffd3030553f50a9e6d4a759970
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2017-03-28 23:39:53 +0200

    erlang-style become (segfaults)

--- src/nimoy.nim
+++ src/nimoy.nim
@@ -14,11 +14,11 @@ type
     sender:   ActorRef[A]
 
   ActorBehavior[A] =
-    proc(context: ActorRef[A], envelope: Envelope[A])
+    proc(self: ActorRef[A], envelope: Envelope[A]): Effect[A]
+
+  Effect[A] = proc(behavior: ActorBehavior[A]): ActorBehavior[A]
 
 
-proc nop[A](self: ActorRef[A], envelope: Envelope[A]) =
-  writeLine(stdout, "Unitialized actor could not handle message ", envelope)
 
 proc send[A](self: ActorRef[A], message: A, receiver: ActorRef[A]) =
   let e = Envelope(
@@ -27,8 +27,17 @@ proc send[A](self: ActorRef[A], message: A, receiver: ActorRef[A]) =
   )
   cast[Actor](receiver).mailbox.send(e)
 
-proc become[A](actor: ActorRef[A], newBehavior: ActorBehavior[A]) =
-  cast[Actor[A]](actor).behavior = newBehavior
+proc stay[A](oldBehavior: ActorBehavior[A]): ActorBehavior[A] = oldBehavior
+proc become[A](newBehavior: ActorBehavior[A]): Effect[A] =
+  return proc(oldBehavior: ActorBehavior[A]): ActorBehavior[A] = newBehavior
+
+proc nop[A](self: ActorRef[A], envelope: Envelope[A]): Effect[A] =
+  writeLine(stdout, "Unitialized actor could not handle message ", envelope)
+  stay[A]
+
+
+# proc become[A](actor: ActorRef[A], newBehavior: ActorBehavior[A]) =
+#   cast[Actor[A]](actor).behavior = newBehavior
 
 proc send[A](actor: ActorRef[A], envelope: Envelope[A]) =
   cast[Actor[A]](actor).mailbox.send(envelope)
@@ -36,12 +45,12 @@ proc send[A](actor: ActorRef[A], envelope: Envelope[A]) =
 proc send[A](actor: Actor[A], envelope: Envelope[A]) =
   actor.mailbox.send(envelope)
 
-proc createActor[A](init: proc(self: ActorRef[A])): auto =
+proc createActor[A](init: proc(self: ActorRef[A]): ActorBehavior[A]): auto =
   var actor = cast[Actor[A]](allocShared0(sizeof(ActorObj[A])))
   actor.mailbox.open()
-  actor.behavior = nop
+  actor.behavior = nop[A]
   let actorRef = cast[ActorRef[A]](actor)
-  init(actorRef)
+  actor.behavior = init(actorRef)
   actorRef
 
 
@@ -52,33 +61,34 @@ proc toTask[A](actorRef: ActorRef[A]): auto =
     if hasMsg:
       actor.behavior(actorRef, msg)
 
-
-
 when isMainModule:
   var executor = createExecutor()
 
-  let foo = createActor[int] do (self: ActorRef[int]):
+  let foo = createActor[int] do (self: ActorRef[int]) -> ActorBehavior[int]:
     var count = 0
-    proc done(self: ActorRef[int], e: Envelope) =
+    proc done(self: ActorRef[int], e: Envelope): Effect[Int] =
       writeLine(stdout, "DISCARD.")
+      stay[int]
 
-    proc receive(self: ActorRef[int], e: Envelope) =
+    proc receive(self: ActorRef[int], e: Envelope): Effect[Int] =
       writeLine(stdout,
         "foo has received ", e.message)
       e.sender.send(Envelope(message: e.message + 1, sender: self))
       count += 1
       if count >= 10:
-        self.become(done)
+        become(done)
+      else:
+        stay[int]
 
-    self.become(receive)
+    return receive
 
-  let bar = createActor do (self: ActorRef[int]):
+  let bar = createActor[int] do (self: ActorRef[int]) -> ActorBehavior[int]:
     proc receive(self: ActorRef[int], e: Envelope[int]) =
       writeLine(stdout,
         "bar has received ", e.message)
       e.sender.send(Envelope[int](message: e.message + 1, sender: self))
 
-    self.become(receive)
+    return receive
 
 
 
