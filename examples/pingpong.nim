4e932ad776544870fdba9ecd39002d0ce2dc50da
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2017-03-29 16:28:31 +0200

    Update pingpong.nim

--- examples/pingpong.nim
+++ examples/pingpong.nim
@@ -1,11 +1,9 @@
 import nimoy, nimoypkg/tasks
 
-# ping receives at least 10 msgs then becomes "done"
 let ping = createActor[int] do (self: ActorRef[int], e: Envelope[int]):
   echo "ping has received ", e.message
   e.sender.send(Envelope[int](message: e.message + 1, sender: self))
 
-# pong responds to ping
 let pong = createActor do (self: ActorRef[int], e: Envelope[int]):
   echo "pong has received ", e.message
   e.sender.send(Envelope[int](message: e.message + 1, sender: self))
