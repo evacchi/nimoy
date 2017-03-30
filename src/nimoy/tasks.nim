6e5d20695dc500de27f2ba97ededfeed4cd5d45e
Vacchi Edoardo (UniCredit) <edoardo.vacchi@unicredit.eu>
2017-03-30 15:40:46 +0200

    begin work on an actual scheduler

--- src/nimoy/tasks.nim
+++ src/nimoy/tasks.nim
@@ -1,57 +1,47 @@
-import locks
-
 type
-  Task*    = proc() {.gcsafe.}
+  Task* = proc() {.gcsafe.}
 
   WorkerId* = int
-
   WorkerObj* = object
     id:      WorkerId
     channel: Channel[Task]
     thread:  Thread[Worker]
-
+    parent:  Executor
   Worker = ptr WorkerObj
 
   ExecutorObj* = object
     workers: seq[Worker]
     channel: Channel[Task]
     thread:  Thread[Executor]
-
   Executor* = ptr ExecutorObj
 
 
-proc workerLoop(worker: Worker) {.gcsafe.} =
-  echo "worker #", $(worker[].id), " has started"
-  var tasks: seq[Task] = @[]
+proc submit*(executor: Executor, task: Task) =
+  executor.channel.send(task)
+
+proc workerLoop(self: Worker) {.gcsafe.} =
   while true:
-    let (hasTask, t) = worker[].channel.tryRecv()
+    let (hasTask, t) = self.channel.tryRecv()
     if (hasTask):
-      echo "worker #", $(worker[].id), " got new task"
-      tasks.add(t)
       t()
-    else:
-      for t in tasks:
-        t()
+      self.parent.submit(t)
 
-
-proc createWorker*(id: int): Worker =
+proc createWorker*(id: int, parent: Executor): Worker =
   result = cast[Worker](allocShared0(sizeof(WorkerObj)))
   result.id = id
   result.channel.open()
+  result.parent = parent
   createThread(result.thread, workerLoop, result)
 
 proc submit*(worker: Worker, task: Task) =
-  echo "task submitted to worker"
   worker.channel.send(task)
-
 
 proc executorLoop(executor: Executor) {.gcsafe.} =
   echo "executor has started"
+  var workerId = 0
   while true:
     let (hasTask, t) = executor.channel.tryRecv()
-    var workerId = 0
     if (hasTask):
-      echo "executor got new task"
       executor.workers[workerId].submit(t)
       workerId = (workerId + 1) mod executor.workers.len
       
@@ -60,13 +50,9 @@ proc createExecutor*(workers: int): Executor =
   result.workers = @[]
   result.channel.open()
   for i in 0..<workers:
-    result.workers.add(createWorker(i))
+    let w = createWorker(i, result) 
+    result.workers.add(w)
   createThread(result.thread, executorLoop, result)
-
-proc submit*(executor: Executor, task: Task) =
-  echo "task submitted"
-  executor.channel.send(task)
-
 
 proc join*(executor: Executor) =
   executor.thread.joinThread()
