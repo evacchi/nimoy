572e2549de93c6f1dec1b6b6616c227646c2ab33
Vacchi Edoardo (UniCredit) <edoardo.vacchi@unicredit.eu>
2017-04-10 11:48:11 +0200

    working graph topology

--- examples/topology.nim
+++ examples/topology.nim
@@ -1,10 +1,9 @@
 import nimoy, future
 
 type  
-  ActorNodeInit[In, Out] =
+  ActorNode[In, Out] =
     proc(self: ActorRef[In], subscriber: ActorRef[Out])
     
-  
 #   Edge[In,X,Out] = tuple
 #     left: ActorNode[In,X]
 #     right: ActorNode[X,Out]  
@@ -12,48 +11,63 @@ type
 # proc `~>`[A,X,B](left: ActorNode[A,X], right: ActorNode[X,B]): Edge[A,X,B] =
 #   (left, right)
 
-proc initNode[In,Out](system: ActorSystem, actorNodeBehavior: ActorNodeInit[In, Out], subscriber: ActorRef[Out]): ActorRef[In] =
-  system.initActor() do (self: ActorRef[In]):
-    actorNodeBehavior(self, subscriber)
+proc initNode[In,Out](
+  system: ActorSystem, 
+  actorNodeBehavior: 
+  ActorNode[In, Out], 
+  subscriber: ActorRef[Out]): ActorRef[In] =
+  system.initActor(
+    (self: ActorRef[In]) => 
+      actorNodeBehavior(self, subscriber))
 
-
-let system = createActorSystem()
-
-let noSender = system.createActor do (self: ActorRef[int], m: int):
-  discard
-let deadLetters = system.createActor do (self: ActorRef[int], m: int):
-  discard
-  
-proc mapNode[In,Out]( f: proc(x:In): Out ): ActorNodeInit[In,Out] = 
-  return proc (self: ActorRef[In], subscriber: ActorRef[Out]) =
-        self.become do (inp: In):
-          subscriber.send(f(inp))
+proc mapNode[In,Out]( f: proc(x:In): Out ): ActorNode[In,Out] = 
+  (self: ActorRef[In], subscriber: ActorRef[Out]) => 
+    self.become((inp: In) => subscriber.send(f(inp)))
 
 proc sinkNode[In]( f: proc(x:In) ): ActorInit[In] =
-  return proc(self: ActorRef[In]) =
-      self.become do (inp: In):
-        f(inp)
+  (self: ActorRef[In]) =>
+    self.become((inp: In) => f(inp))
+
+proc fanOutNode[In](node1: ActorRef[In], node2: ActorRef[In]): ActorInit[In] =
+  (self: ActorRef[In]) => 
+    self.become do (inp: In):
+      node1.send(inp)
+      node2.send(inp)
+
+# nodes
+let sink1: ActorInit[int] = sinkNode[int]( (x: int) => echo("sink1 = ", x) )
+let sink2: ActorInit[int] = sinkNode[int]( (x: int) => echo("sink2 = ", x) )
+
+let fanIn: ActorNode[int, int] = mapNode[int,int] do (x:int) -> int:
+  echo("fan in = ", x) 
+  x
+
+let map3: ActorNode[int, int] = mapNode[int,int]( (x: int) => x+7 )
+let map2: ActorNode[int, int] = mapNode[int,int]( (x: int) => x-1 )
+let map1: ActorNode[int, int] = mapNode[int,int]( (x: int) => x*2 )
+
+# instantiate topology
+let system = createActorSystem()
+
+#
+# source ~> map1 ~> fanIn ~> map3 ~> fanOut ~> sink1
+# source ~> map2 ~~~~~^                 +~~~~> sink2
+#
 
 
+let sink1Ref = system.initActor(sink1)
+let sink2Ref = system.initActor(sink2)
+let fanOut: ActorInit[int] = fanOutNode[int]( sink1Ref, sink2Ref )
+let fanOutRef = system.initActor(fanOut)
 
-let sink: ActorInit[int] = sinkNode[int]( (x: int) => echo(x) )
-let map2: ActorNodeInit[int, int] = mapNode[int,int]( (x: int) => x-1 )
-let map1: ActorNodeInit[int, int] = mapNode[int,int]( (x: int) => x*2 )
+let map3Ref = system.initNode(map3, fanOutRef)
+let fanInRef = system.initNode(fanIn, map3Ref)
+let map2Ref = system.initNode(map2, fanInRef)
+let map1Ref = system.initNode(map1, fanInRef)
 
-let sinkRef = system.initActor(sink)
-let map2Ref = system.initNode(map2, sinkRef)
-let map1Ref = system.initNode(map1, map2Ref)
-
-# send a tick
+# send input
 for i in 0..10:
-  map1Ref.send(i)
-
-
-# for i in 0..10:
-#   map1Ref.send(Envelope[int](message: i, sender: noSender))
-
-# let mapNode    = node(map[int]())
-# let sourceNode = node(sink[int]())
+  map1Ref ! i
 
 system.awaitTermination()
 
