21c89f5c9002021541dcca626c25665b93e5a8b4
Vacchi Edoardo (UniCredit) <edoardo.vacchi@unicredit.eu>
2017-04-10 13:46:22 +0200

    simpler api

--- examples/topology.nim
+++ examples/topology.nim
@@ -3,6 +3,9 @@ import nimoy, future
 type  
   ActorNode[In, Out] =
     proc(self: ActorRef[In], subscriber: ActorRef[Out])
+  
+  Topology = object
+    system: ActorSystem
     
 #   Edge[In,X,Out] = tuple
 #     left: ActorNode[In,X]
@@ -11,20 +14,27 @@ type
 # proc `~>`[A,X,B](left: ActorNode[A,X], right: ActorNode[X,B]): Edge[A,X,B] =
 #   (left, right)
 
+proc createTopology(system: ActorSystem): Topology =
+  Topology(system: system)
+
+proc createTopology(): Topology =
+  Topology(system: createActorSystem())
+
+
 proc initNode[In,Out](
-  system: ActorSystem, 
+  topology: Topology, 
   actorNode: ActorNode[In, Out], 
   subscriber: ActorRef[Out]
 ): ActorRef[In] =
-  system.initActor(
+  topology.system.initActor(
     (self: ActorRef[In]) => 
       actorNode(self, subscriber))
 
-proc createFanOutRef[In](
-  system: ActorSystem, 
+proc broadcastRef[In](
+  topology: Topology, 
   subscribers: varargs[ActorRef[In]]): ActorRef[In] =
   let subs = @subscribers
-  system.createActor do (self: ActorRef[In], m: In): 
+  topology.system.createActor do (self: ActorRef[In], m: In): 
       for s in subs:
         s ! m
 
@@ -36,38 +46,39 @@ proc sinkNode[In]( f: proc(x:In) ): ActorInit[In] =
   (self: ActorRef[In]) =>
     self.become((inp: In) => f(inp))
 
-proc fanOutNode[In](node1: ActorRef[In], node2: ActorRef[In]): ActorInit[In] =
+proc broadcastNode[In](node1: ActorRef[In], node2: ActorRef[In]): ActorInit[In] =
   (self: ActorRef[In]) => 
     self.become do (inp: In):
       node1.send(inp)
       node2.send(inp)
 
-proc createSinkRef[In](system: ActorSystem, f: proc(x:In) ): ActorRef[In] =
-   system.initActor(sinkNode(f))
+proc sinkRef[In](topology: Topology, f: proc(x:In) ): ActorRef[In] =
+   topology.system.initActor(sinkNode(f))
 
-proc createNodeRef[In,Out](system: ActorSystem, f: proc(x:In): Out, subscriber: ActorRef[Out]): ActorRef[In] =
-   system.initNode(node[In,Out](f), subscriber)
+proc nodeRef[In,Out](topology: Topology, f: proc(x:In): Out, subscriber: ActorRef[Out]): ActorRef[In] =
+   topology.initNode(node[In,Out](f), subscriber)
 
 
-# instantiate topology
-let system = createActorSystem()
 
 #
-# source ~> map1 ~> fanIn ~> map3 ~> fanOut ~> sink1
+# source ~> map1 ~> fanIn ~> map3 ~> broadcast ~> sink1
 #    +~~~~> map2 ~~~~~^                 +~~~~> sink2
 #  
 
-let sink1Ref  = system.createSinkRef((x: int) => echo("sink1 = ", x))
-let sink2Ref  = system.createSinkRef((x: int) => echo("sink2 = ", x))
-let fanOutRef = system.createFanOutRef(sink1Ref, sink2Ref)
-let map3Ref   = system.createNodeRef((x: int) => x+7, fanOutRef)
-let fanInRef  = system.createNodeRef((x: int) => ( echo("fan in = ", x); x ), map3Ref)
-let map2Ref   = system.createNodeRef((x: int) => x-1, fanInRef)
-let map1Ref   = system.createNodeRef((x: int) => x*2, fanInRef)
+
+let t = createTopology()
+
+let sink1Ref = t.sinkRef((x: int) => echo("sink1 = ", x))
+let sink2Ref = t.sinkRef((x: int) => echo("sink2 = ", x))
+let bref     = t.broadcastRef(sink1Ref, sink2Ref)
+let map3Ref  = t.nodeRef((x: int) => x+7,  bref)
+let fanInRef = t.nodeRef((x: int) => ( echo("fan in = ", x); x ), map3Ref)
+let map2Ref  = t.nodeRef((x: int) => x-1, fanInRef)
+let map1Ref  = t.nodeRef((x: int) => x*2, fanInRef)
 
 # send input
 for i in 0..10:
   map1Ref ! i
 
-system.awaitTermination()
+t.system.awaitTermination()
 
