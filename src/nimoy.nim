448761d15b4af107584d3491616d41944ec5ad9c
Vacchi Edoardo (UniCredit) <edoardo.vacchi@unicredit.eu>
2017-04-19 18:10:20 +0200

    Squashed commit of the following:
    commit 605d971e2b2ad640f79079f6aedef6007628949c
    Author: Vacchi Edoardo (UniCredit) <edoardo.vacchi@unicredit.eu>
    Date:   Wed Apr 19 18:09:52 2017 +0200
    
        updated topology example with channel
    
    commit 16d10210f96ed27b13fba53467df6a76e15b63da
    Author: Vacchi Edoardo (UniCredit) <edoardo.vacchi@unicredit.eu>
    Date:   Wed Apr 19 17:38:39 2017 +0200
    
        api: new channel api

--- src/nimoy.nim
+++ src/nimoy.nim
@@ -1,21 +1,6 @@
 import nimoy/tasks, nimoy/executors
 
 type
-  ActorObj*[A] = object
-    sysbox: Channel[SystemMessage]
-    mailbox: Channel[A]
-    behavior: ActorBehavior[A]
-
-  Actor*[A] = ptr ActorObj[A]
-  ActorRef*[A] = object
-    actor: Actor[A]
-
-  SystemMessage* = enum
-    sysKill
-    
-  Envelope*[A] = object
-    message*:  A
-    sender*:   ActorRef[A]
 
   ActorInit*[A] =
     proc(self: ActorRef[A])
@@ -24,7 +9,27 @@ type
     proc(message: A)
 
   ActorContextBehavior*[A] =
-    proc(self: ActorRef[A], message: A)
+    proc(ctx: ActorRef[A], message: A)
+
+  ActorChannelObj[A] = object
+    channel:  Channel[A]
+    behavior: ActorBehavior[A]
+
+  ActorChannel[A] = ptr ActorChannelObj[A]
+
+  SystemMessage* = enum
+    sysKill
+  
+  Actor*[A] = object
+    sys:  ActorChannel[SystemMessage]
+    main: ActorChannel[A]
+
+  ActorRef*[A] = object
+    actor: Actor[A]
+    
+  Envelope*[A] = object
+    message*:  A
+    sender*:   ActorRef[A]
 
   ActorSystem* = object
     executor: Executor
@@ -40,16 +45,25 @@ proc send*[A](receiver: ActorRef[A], message: A, sender: ActorRef[A]) =
   receiver.actor.mailbox.send(e)
 
 proc become*[A](actorRef: ActorRef[A], newBehavior: ActorBehavior[A]) =
-  actorRef.actor.behavior = newBehavior
+  actorRef.actor.main.behavior = newBehavior
+
+proc send*[A](actorChannel: ActorChannel[A], message: A) =
+  actorChannel.channel.send(message)
+
+proc tryRecv*[A](actorChannel: ActorChannel[A]): tuple[dataAvailable: bool, msg: A] =
+  actorChannel.channel.tryRecv()
+
+proc recv*[A](actorChannel: ActorChannel[A]): A =
+  actorChannel.channel.recv()
 
 proc send*[A](actor: Actor[A], message: A) =
-  actor.mailbox.send(message)
+  actor.main.send(message)
 
 proc send*[A](actorRef: ActorRef[A], message: A) =
   actorRef.actor.send(message)
 
 proc send*[A](actor: Actor[A], sysMessage: SystemMessage) =
-  actor.sysbox.send(sysMessage)
+  actor.sys.send(sysMessage)
 
 proc send*[A](actorRef: ActorRef[A], sysMessage: SystemMessage) =
   actorRef.actor.send(sysMessage)
@@ -57,17 +71,27 @@ proc send*[A](actorRef: ActorRef[A], sysMessage: SystemMessage) =
 template `!`*(receiver, message: untyped) =
   receiver.send(message)
 
+proc allocActorChannel*[A](): ActorChannel[A] =
+  result = cast[ActorChannel[A]](allocShared0(sizeof(ActorChannelObj[A])))
+  result.channel.open()
+  result.behavior = nop
+
+proc destroyActorChannel*[A](actorChannel: ActorChannel[A]) =
+  deallocShared(actorChannel)
+
+proc allocActor[A](): Actor[A] =
+  result.main = allocActorChannel[A]()
+  result.sys  = allocActorChannel[SystemMessage]()
+
+proc destroyActor*[A](actor: Actor[A]) =
+  deallocShared(actor.main)
+  deallocShared(actor.sys)
+
 proc createActor*[A](init: ActorInit[A]): ActorRef[A] =
-  var actor = cast[Actor[A]](allocShared0(sizeof(ActorObj[A])))
-  actor.sysbox.open()
-  actor.mailbox.open()
-  actor.behavior = nop
+  var actor = allocActor[A]()
   let actorRef = ActorRef[A](actor: actor)
   init(actorRef)
   actorRef
-
-proc destroyActor*[A](actor: Actor[A]) =
-  deallocShared(actor)
 
 proc createActor*[A](receive: ActorBehavior[A]): ActorRef[A] =    
   proc init(self: ActorRef[A]) =
@@ -85,16 +109,16 @@ proc createActor*[A](receive: ActorContextBehavior[A]): ActorRef[A] =
 proc toTask*[A](actorRef: ActorRef[A]): Task =
   return proc(): TaskStatus {.gcsafe.} =
     let actor = actorRef.actor
-    let (hasSysMsg, sysMsg) = actor.sysbox.tryRecv()
+    let (hasSysMsg, sysMsg) = actor.sys.tryRecv()
     if hasSysMsg:
       case sysMsg
       of sysKill:
         destroyActor(actorRef.actor)
         taskFinished
     else:
-      let (hasMsg, msg) = actor.mailbox.tryRecv()
+      let (hasMsg, msg) = actor.main.tryRecv()
       if hasMsg:
-        actor.behavior(msg)
+        actor.main.behavior(msg)
       taskContinue
 
 proc createActorSystem*(executor: Executor): ActorSystem =
